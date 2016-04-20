0daf84ffcf54c024e9a433ccf8efa62696eda056
Rhys Van Orsouw <rhysvo@gmail.com>
2016-04-20 11:31:20 +1000

    Renamed methods and variables for clearer code

--- Problem2/Problem2.py
+++ Problem2/Problem2.py
@@ -1,15 +1,17 @@
-cache = {}
+fib_numbers = {}
 
 
-def fib(n):
-    cache[n] = cache.get(n, 0) or (n <= 1 and 1 or fib(n - 1) + fib(n - 2))
-    return cache[n]
+# Recursive Method for finding fibonacci
+def fib_sequence(n):
+    fib_numbers[n] = fib_numbers.get(n, 0) or \
+                     (n <= 1 and 1 or fib_sequence(n - 1) + fib_sequence(n - 2))
+    return fib_numbers[n]
 
 
 n = 0
 i = 0
-while fib(i) <= 4000000:
-    if not fib(i) % 2:
-        n = n + fib(i)
-    i = i + 1
+while fib_sequence(i) <= 4000000:
+    if not fib_sequence(i) % 2:
+        n = n + fib_sequence(i)
+    i += 1
 print(n)
